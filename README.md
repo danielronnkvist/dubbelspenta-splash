69df414c05e87ca9a308eae7d607bdb32bde5182
danielronnkvist <daniel@ronnkvist.com>
2014-04-15 09:58:20 +0200

    This is it.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ dubbelspenta-splash
 ===================
 
 A dirty DIRTY splash screen used on dubbelspentasittningen 22/3
+
+Hashtag pull taken from https://github.com/jfrazelle/hashtag-pull/
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+date_default_timezone_set('America/New_York');
+
+$twitter = array(
+    'consumer_key' =>           '',
+    'consumer_secret' =>        '',
+    'access_token' =>           '',
+    'access_token_secret' =>    ''
+);
+
+$instagram = array(
+    'client_id' =>       '',
+    'client_secret' =>  '',
+    'website_url' =>    '',
+    'redirect_uri' =>   '',
+    'access_token' =>   ''
+);
+
+$db = array(
+    'host' =>       'localhost',
+    'user' =>       'root', 
+    'password' =>   '', 
+    'name' =>       'dubbelspenta'
+);
+
+$hashtag = 'dubbelspenta';
+
+?>
